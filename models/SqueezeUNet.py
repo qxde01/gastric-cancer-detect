647a4e68a23d316698e9b3a5a292b83aa0913e4b
qxde01 <qxde01@gmail.com>
2020-09-20 20:07:53 +0800

    Add files via upload

---
+++ models/SqueezeUNet.py
@@ -0,0 +1,116 @@
+from tensorflow import keras
+
+sq1x1 = "squeeze1x1"
+exp1x1 = "expand1x1"
+exp3x3 = "expand3x3"
+relu = "relu_"
+# Modular function for Fire Node
+# https://github.com/rcmalli/keras-squeezenet/blob/master/keras_squeezenet/squeezenet.py
+def fire_module(x, fire_id, squeeze=16, expand=64):
+    s_id = 'fire' + str(fire_id) + '/'
+    channel_axis = 3
+    x = keras.layers.Conv2D(squeeze, (1, 1), padding='valid', use_bias=True,kernel_initializer='he_normal',name=s_id + sq1x1)(x)
+    x = keras.layers.BatchNormalization(axis=-1,momentum=0.95,epsilon=1e-5, name=s_id + sq1x1+'/bn')(x)
+    #x = keras.layers.Activation('relu', name=s_id + relu + sq1x1)(x)
+    x=keras.layers.LeakyReLU()(x)
+    left = keras.layers.Conv2D(expand//2, (1, 1), padding='valid',  use_bias=True,kernel_initializer='he_normal',name=s_id + exp1x1)(x)
+    left = keras.layers.BatchNormalization(axis=-1, momentum=0.95,epsilon=1e-5,name=s_id + exp1x1 + '/bn')(left)
+    #left = keras.layers.Activation('relu', name=s_id + relu + exp1x1)(left)
+    left = keras.layers.LeakyReLU()(left)
+    right = keras.layers.Conv2D(expand//2, (3, 3), padding='same', use_bias=True,kernel_initializer='he_normal', name=s_id + exp3x3)(x)
+    right = keras.layers.BatchNormalization(axis=-1,momentum=0.95,epsilon=1e-5,name=s_id + exp3x3 + '/bn')(right)
+    #right = keras.layers.Activation('relu', name=s_id + relu + exp3x3)(right)
+    right = keras.layers.LeakyReLU()(right)
+    x = keras.layers.concatenate([left, right], axis=channel_axis, name=s_id + 'concat')
+    #print(x)
+    return x
+
+
+def SqueezeUNet(input_shape=(256,256,3)):
+    """Instantiates the SqueezeNet architecture.
+    """
+    filters=[96,128,256,384,512]
+    filters = [48, 64, 128, 192, 256]
+    img_input = keras.layers.Input(shape=input_shape)
+    x = keras.layers.Conv2D(filters[0], (3, 3), strides=(1, 1), padding='same',  use_bias=True,kernel_initializer='he_normal',name='conv1')(img_input) #shape=(None, 256, 256, 96)
+    x=keras.layers.BatchNormalization(axis=-1,momentum=0.95,epsilon=1e-5,name='conv1_bn')(x)
+    x = keras.layers.LeakyReLU()(x)
+    x = keras.layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2), name='pool1')(x) # shape=(None, 128, 128, 96)
+
+    f1 = fire_module(x, fire_id=2, squeeze=16, expand=filters[1]) #shape=(None, 128, 128, 128)
+    f2 = fire_module(f1, fire_id=3, squeeze=16, expand=filters[1]) #shape=(None, 128, 128, 128)
+    f2= keras.layers.add([f2,f1],name='fire2_3') #shape=(None, 128, 128, 128)
+    f3= fire_module(f2, fire_id=4, squeeze=32, expand=filters[2])  # shape=(None, 128, 128, 256)
+    f4 = keras.layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2), name='pool3',padding='same')(f3) #shape=(None, 64, 64, 256)
+
+    f5 = fire_module(f4, fire_id=5, squeeze=32, expand=filters[2])  # shape=(None, 64, 64, 256)
+    f5 = keras.layers.add([f5, f4], name='fire4_5')
+    f6=fire_module(f5, fire_id=6, squeeze=48, expand=filters[3]) #shape=(None, 64, 64, 384)
+
+    f7 = keras.layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2), name='pool4', padding='same')(f6)
+
+
+    f8 = fire_module(f7, fire_id=7, squeeze=48, expand=filters[3]) #shape=(None, 32, 32, 384) d
+    f9 = keras.layers.add([f7,f8] ,name='fire5_6')   #
+    f9 = fire_module(f9, fire_id=8, squeeze=64, expand=filters[4])  # shape=(None, 32, 32, 512
+
+    f10 = keras.layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2), name='pool8')(f9) #shape=(None, 16, 16, 512)
+    #x = fire_module(x, fire_id=8, squeeze=64, expand=256) #13, 13, 512
+    f10 = fire_module(f10, fire_id=9, squeeze=64, expand=filters[4]) # shape=(None, 16, 16, 512)
+
+    up5=keras.layers.Conv2DTranspose(filters[4],kernel_size=3,strides=(2,2),padding='same')(f10) #shape=(None, 32, 32, 512)
+    up5d=keras.layers.Add()([up5,f9])
+    up5d = fire_module(up5d, fire_id=10, squeeze=48, expand=filters[3]) #shape=(None, 32, 32, 384)
+
+    up4 = keras.layers.Conv2DTranspose(filters[3], kernel_size=3, strides=(2, 2), padding='same')(up5d) #shape=(None, 64, 64, 384)
+    up4d = keras.layers.Add()([up4, f6])
+    up4d = fire_module(up4d, fire_id=11, squeeze=32, expand=filters[2])
+
+    up3 = keras.layers.Conv2DTranspose(filters[2], kernel_size=3, strides=(2, 2), padding='same')(up4d) #shape=(None, 128, 128, 256)
+    up3d = keras.layers.Add()([up3, f3])
+
+    up3d = fire_module(up3d, fire_id=12, squeeze=16, expand=filters[1])
+    #up3d = fire_module(up3d, fire_id=13, squeeze=16, expand=128)
+
+    up20 = keras.layers.Conv2DTranspose(filters[1], kernel_size=3, strides=(2, 2), padding='same')(up3d)
+    up2 = keras.layers.Conv2D(filters[0], (3, 3), strides=(1, 1), padding='same', use_bias=True, kernel_initializer='he_normal', name='conv_up2')(up20)
+    up2 = keras.layers.BatchNormalization(axis=-1, momentum=0.95, epsilon=1e-5, name='conv_up2_bn')(up2)
+    up2 = keras.layers.LeakyReLU()(up2)
+
+    d1 = keras.layers.Conv2D(1, (3, 3), padding="same", activation=None, use_bias=False)(up2)
+    d11 = keras.layers.Activation('sigmoid', name='d1')(d1)
+
+    d2 = keras.layers.Conv2D(1, (3, 3), padding="same", activation=None, use_bias=False)(up20)
+    d22 = keras.layers.Activation('sigmoid', name='d2')(d2)
+
+    d3 = keras.layers.Conv2DTranspose(filters[1], kernel_size=3, strides=(2, 2), padding='same')(up3d)
+    d3 = keras.layers.Conv2D(1, (3, 3), padding="same", activation=None, use_bias=False)(d3)
+    d33 = keras.layers.Activation('sigmoid', name='d3')(d3)
+
+    d4 = keras.layers.Conv2DTranspose(filters[2], kernel_size=3, strides=(2, 2), padding='same')(up4d)
+    #d4 = keras.layers.Conv2DTranspose(256, kernel_size=3, strides=(2, 2), padding='same')(d4)
+    d4 = keras.layers.UpSampling2D(size=(2, 2))(d4)
+    d4 = keras.layers.Conv2D(1, (3, 3), padding="same", activation=None, use_bias=False)(d4)
+    d44 = keras.layers.Activation('sigmoid', name='d4')(d4)
+
+    d5 = keras.layers.Conv2DTranspose(filters[3], kernel_size=3, strides=(2, 2), padding='same')(up5d)
+    #d5 = keras.layers.Conv2DTranspose(384, kernel_size=3, strides=(2, 2), padding='same')(d5) #1327488
+    d5 = keras.layers.Conv2DTranspose(filters[3], kernel_size=3, strides=(2, 2), padding='same')(d5) #1327488
+    d5=keras.layers.UpSampling2D(size=(2,2))(d5)
+    d5 = keras.layers.Conv2D(1, (3, 3), padding="same", activation=None, use_bias=False)(d5)
+    d55 = keras.layers.Activation('sigmoid', name='d5')(d5)
+
+    d = keras.layers.concatenate([d1, d2, d3, d4, d5, img_input])
+    d = keras.layers.Conv2D(1, kernel_size=3, activation=None, padding='same', use_bias=False)(d)
+    d = keras.layers.Activation('sigmoid', name='d')(d)
+
+    model = keras.models.Model(inputs=img_input, outputs=[d, d11, d22, d33, d44, d55])
+
+    return model
+
+
+if __name__ == "__main__":
+    #Total params: 10,792,696 8,862,120
+    model=SqueezeUNet(input_shape=(256,256,3))
+    model.summary()
+    keras.utils.plot_model(model, 'SqueezeUNet.png', show_shapes=True)
---
+++ models/UMFacenet2.py
@@ -0,0 +1,181 @@
+from tensorflow.keras import backend as K
+from tensorflow import keras
+
+"""Building Block Functions"""
+
+
+def se_block(inputs, reduction=16):
+    channel_axis = 1 if K.image_data_format() == "channels_first" else -1
+    filters = inputs.shape[channel_axis]
+    nn = keras.layers.GlobalAveragePooling2D()(inputs)
+    nn = keras.layers.Reshape((1, 1, filters))(nn)
+    nn = keras.layers.Conv2D(filters // reduction, kernel_size=1)(nn)
+    # nn = keras.layers.PReLU(shared_axes=[1, 2])(nn)
+    nn = keras.layers.LeakyReLU()(nn)
+    nn = keras.layers.Conv2D(filters, kernel_size=1, activation="sigmoid")(nn)
+    nn = keras.layers.Multiply()([inputs, nn])
+    return nn
+
+
+def se_block_2(inputs, reduction=16):
+    channel_axis = 1 if K.image_data_format() == "channels_first" else -1
+    filters = inputs.shape[channel_axis]
+    se = keras.layers.GlobalAveragePooling2D()(inputs)
+    se = keras.layers.Dense(filters // reduction, activation=None, use_bias=False)(se)
+    se=keras.layers.LeakyReLU()(se)
+    se = keras.layers.Dense(filters, activation="sigmoid", use_bias=False)(se)
+    # if K.image_data_format() == 'channels_first':
+    #     se = Permute((3, 1, 2))(se)
+    x = keras.layers.Multiply()([inputs, se])
+    return x
+
+
+def conv_block(inputs, filters, kernel_size, strides, padding):
+    channel_axis = 1 if K.image_data_format() == "channels_first" else -1
+    Z = keras.layers.Conv2D(filters, kernel_size, strides=strides, padding=padding, use_bias=False)(inputs)
+    Z = keras.layers.BatchNormalization(axis=channel_axis)(Z)
+    # A = keras.layers.PReLU(shared_axes=[1, 2])(Z)
+    A = keras.layers.LeakyReLU()(Z)
+    return A
+
+
+def separable_conv_block(inputs, filters, kernel_size, strides):
+    channel_axis = 1 if K.image_data_format() == "channels_first" else -1
+    Z = keras.layers.SeparableConv2D(filters, kernel_size, strides=strides, padding="same", use_bias=False)(inputs)
+    Z = keras.layers.BatchNormalization(axis=channel_axis)(Z)
+    # A = keras.layers.PReLU(shared_axes=[1, 2])(Z)
+    A = keras.layers.LeakyReLU()(Z)
+    return A
+
+
+def bottleneck(inputs, filters, kernel, t, s, r=False, se=False):
+    channel_axis = -1 #1 if K.image_data_format() == "channels_first" else -1
+    tchannel = K.int_shape(inputs)[channel_axis] * t
+    Z1 = conv_block(inputs, tchannel, 1, 1, "same")
+    if s==2:
+        Z1=keras.layers.MaxPooling2D(pool_size=(2,2))(Z1)
+    else:
+        Z1 = keras.layers.DepthwiseConv2D(kernel, strides=s, padding="same", depth_multiplier=1, use_bias=False)(Z1)
+    Z1 = keras.layers.BatchNormalization(axis=channel_axis)(Z1)
+    # A1 = keras.layers.PReLU(shared_axes=[1, 2])(Z1)
+    A1 = keras.layers.LeakyReLU()(Z1)
+    Z2 = keras.layers.Conv2D(filters, 1, strides=1, padding="same", use_bias=False)(A1)
+    Z2 = keras.layers.BatchNormalization(axis=channel_axis)(Z2)
+    if se:
+        Z2 = se_block(Z2)
+    if r:
+        Z2 = keras.layers.add([Z2, inputs])
+    return Z2
+
+
+def inverted_residual_block(inputs, filters, kernel, t, strides, n, se=False):
+    Z = bottleneck(inputs, filters, kernel, t, strides, se=se)
+    for i in range(1, n):
+        Z = bottleneck(Z, filters, kernel, t, 1, True, se=se)
+    return Z
+
+
+def linear_GD_conv_block(inputs, kernel_size, strides):
+    channel_axis = 1 if K.image_data_format() == "channels_first" else -1
+    Z = keras.layers.DepthwiseConv2D(kernel_size, strides=strides, padding="valid", depth_multiplier=1, use_bias=False)(inputs)
+    Z = keras.layers.BatchNormalization(axis=channel_axis)(Z)
+    return Z
+
+
+def UMFacenet(input_shape=(256, 256, 3), dropout=0.5, filters=[64, 128, 256, 512], use_se=True):
+    # channel_axis =  -1
+    X = keras.layers.Input(shape=input_shape)
+    M0 = conv_block(X, filters[0], 3, 2, "same")  # 128
+    M = separable_conv_block(M0, filters[0], 3, 1)  # 128
+    M1 = inverted_residual_block(M, filters[0], 3, t=2, strides=2, n=5, se=use_se)  # 64
+    if dropout > 0.:
+        M1 = keras.layers.Dropout(dropout)(M1)
+    M2 = inverted_residual_block(M1, filters[1], 3, t=4, strides=2, n=1, se=use_se)  # 32
+    M2 = inverted_residual_block(M2, filters[1], 3, t=2, strides=1, n=6, se=use_se)  # 32
+    if dropout > 0.:
+        M2 = keras.layers.Dropout(dropout)(M2)
+
+    M3 = inverted_residual_block(M2, filters[2], 3, t=4, strides=2, n=1, se=use_se)  # 16
+    M3 = inverted_residual_block(M3, filters[2], 3, t=2, strides=1, n=2, se=use_se)  # 16
+
+    if dropout > 0.:
+        M3 = keras.layers.Dropout(dropout)(M3)
+
+    # M4 = inverted_residual_block(M3, filters[3], 3, t=4, strides=2, n=1, se=use_se) #8
+    # M4 = inverted_residual_block(M4, filters[3], 3, t=2, strides=1, n=2, se=use_se)
+
+    # up4=keras.layers.UpSampling2D(size=(2,2))(M4)
+    # up4=keras.layers.Conv2DTranspose(filters[3], (3, 3), strides=(2, 2), padding="same",name='up2_trans4d')(M4)
+    # up4d=keras.layers.concatenate([up4,M3])
+    # up4d=inverted_residual_block(up4d, filters[3], 3, t=2, strides=1, n=2, se=use_se)
+    # up4d=inverted_residual_block(up4d, filters[3], 3, t=4, strides=1, n=1, se=use_se) #16
+
+    # up3=keras.layers.UpSampling2D(size=(2,2))(up4d)
+    up3 = keras.layers.Conv2DTranspose(filters[2], (3, 3), strides=(2, 2), padding="same", name='up3_trans2d')(M3)
+    up3d = keras.layers.concatenate([up3, M2])
+    up3d = inverted_residual_block(up3d, filters[2], 3, t=2, strides=1, n=2, se=use_se)  # 32
+    up3d = inverted_residual_block(up3d, filters[2], 3, t=4, strides=1, n=1, se=use_se)
+
+    # up2=keras.layers.UpSampling2D(size=(2,2))(up3d)
+    up2 = keras.layers.Conv2DTranspose(filters[1], (3, 3), strides=(2, 2), padding="same", name='up2_trans2d')(up3d)
+    up2d = keras.layers.concatenate([up2, M1])
+    up2d = inverted_residual_block(up2d, filters[1], 3, t=2, strides=1, n=6, se=use_se)  # 64
+    up2d = inverted_residual_block(up2d, filters[1], 3, t=4, strides=1, n=1, se=use_se)
+
+    # up1 = keras.layers.UpSampling2D(size=(2, 2))(up2d) #128
+    up1 = keras.layers.Conv2DTranspose(filters[0], (3, 3), strides=(2, 2), padding="same", name='up1_trans2d')(up2d)
+    up1d = keras.layers.concatenate([up1, M])
+    up1d = inverted_residual_block(up1d, filters[0], 3, t=2, strides=1, n=5, se=use_se)
+    up1d = separable_conv_block(up1d, filters[0], 3, 1)
+    up1d = conv_block(up1d, filters[0], 3, 1, "same")
+
+    d1 = keras.layers.Conv2DTranspose(filters[0], (3, 3), strides=(2, 2), padding="same", name='d1_trans2d')(up1d)
+    d1 = keras.layers.Conv2D(1, kernel_size=3, activation=None, padding='same', use_bias=False)(d1)
+    d11 = keras.layers.Activation('sigmoid', name='d1')(d1)
+
+    #d2 = keras.layers.Conv2DTranspose(filters[1], (3, 3), strides=(2, 2), padding="same", name='d2_trans2d_1')(up2d)
+    d2 = keras.layers.UpSampling2D(size=(4, 4))(up2d)
+    #d2 = keras.layers.Conv2DTranspose(filters[1], (3, 3), strides=(2, 2), padding="same", name='d2_trans2d_2')(d2)
+    d2 = keras.layers.Conv2D(1, kernel_size=3, activation=None, padding='same', use_bias=False)(d2)
+    d22 = keras.layers.Activation('sigmoid', name='d2')(d2)
+
+    #d3 = keras.layers.Conv2DTranspose(filters[2], (3, 3), strides=(4, 4), padding="same", name='d3_trans2d_1')(up3d)
+    d3 = keras.layers.UpSampling2D(size=(8, 8))(up3d)
+    #d3 = keras.layers.Conv2DTranspose(filters[2], (3, 3), strides=(2, 2), padding="same", name='d3_trans2d_2')(d3)
+    d3 = keras.layers.Conv2D(1, kernel_size=3, activation=None, padding='same', use_bias=False)(d3)
+    d33 = keras.layers.Activation('sigmoid', name='d3')(d3)
+
+    #d4 = keras.layers.Conv2DTranspose(filters[0], (3, 3), strides=(2, 2), padding="same")(M0)
+    d4=keras.layers.UpSampling2D(size=(2,2))(M0)
+    d4 = keras.layers.Conv2D(1, kernel_size=3, activation=None, padding='same', use_bias=False)(d4)
+    d44 = keras.layers.Activation('sigmoid', name='d4')(d4)
+
+    #d5 = keras.layers.Conv2DTranspose(filters[0], (3, 3), strides=(2, 2), padding="same")(M)
+    d5 = keras.layers.UpSampling2D(size=(2, 2))(M)
+    d5 = keras.layers.Conv2D(1, kernel_size=3, activation=None, padding='same', use_bias=False)(d5)
+    d55 = keras.layers.Activation('sigmoid', name='d5')(d5)
+
+    d = keras.layers.concatenate([d1, d2, d3, d4, d5, X])
+    d = keras.layers.Conv2D(1, kernel_size=3, activation=None, padding='same', use_bias=False)(d)
+    d = keras.layers.Activation('sigmoid', name='d')(d)
+
+    model = keras.models.Model(inputs=X, outputs=[d, d11, d22, d33, d44, d55])
+    return model
+
+
+if __name__ == '__main__':
+    '''
+   Total params: 1,615,048
+   Trainable params: 1,592,392
+   Non-trainable params: 22,656
+   -SE
+
+   Total params: 1,571,140
+   Trainable params: 1,548,484
+   Non-trainable params: 22,656
+
+    '''
+    # model=UMFacenet(input_shape=(256,256,3))
+    model = UMFacenet(input_shape=(256, 256, 3), filters=[32, 64, 128, 256], use_se=True)
+    model.summary()
+    keras.utils.plot_model(model, 'UMFacenetS0-SE.png', show_shapes=True)
