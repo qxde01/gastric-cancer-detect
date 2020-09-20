0851a44a9c13595b53f02b38005318a9ebd7273d
qxde01 <qxde01@gmail.com>
2020-09-20 23:58:07 +0800

    Update trainM.py

--- trainM.py
+++ trainM.py
@@ -1,44 +1,45 @@
 from tensorflow import keras
 import pandas as pd
 import sys,argparse
-from models.Unet import Unet
-from models.Unet3 import Unet3
-from models.U2net import U2net,U2netS,U2netM
-from models.UEfficientNet import UEfficientNetB4
+from models import build_model
 from data import DataGeneratorM
-from  utils import IOU,bce_dice_loss,DICE
+from  utils import IOU,DICE
+from  models.losses import getLoss
+from sch import WarmUpLearningRateScheduler,CosineAnnealingScheduler
 import tensorflow_addons as tfa
+#from bert4keras.optimizers import Adam,extend_with_weight_decay,extend_with_gradient_accumulation
 
-def build_model(size=256,net='U2netS'):
-    if net=='U2netS':
-        model = U2netS(input_shape=(size, size, 3), drop_rate=0.0)
-    elif net=='U2netM':
-        model = U2netM(input_shape=(size, size, 3))
-    elif net=='U2net':
-        model = U2net(input_shape=(size, size, 3))
-    elif net=='Unet3':
-        model = Unet3(input_shape=(size, size, 3))
-    elif net=='Unet':
-        model = Unet(input_shape=(size, size, 3))
-    elif net=='UEfficientNetB4':
-        model = UEfficientNetB4(input_shape=(size, size, 3),imagenet_weights='saved/efficientnet-b4_imagenet_1000_notop.h5')
+
+def modelSave(net,size=256,name=None,num_mask=1):
+    '''
+    if name==None:
+        if num_mask==1:
+            filepath = 'saved/%s_%s_{val_loss:.4f}-{val_DICE:.4f}-{epoch:03d}.h5' % (net, size)
+        else:
+            filepath = 'saved/%s_%s_{val_loss:.4f}-{val_d_DICE:.4f}-{epoch:03d}.h5' % (net, size)
     else:
-        print(' not support your net .')
-        sys.exit()
-    return model
+    '''
+    if num_mask==1:
+            filepath = 'saved/%s_%s_%s_{val_loss:.4f}-{val_DICE:.4f}-{epoch:03d}.h5' % (net, size,name)
+    else:
+            filepath = 'saved/%s_%s_%s_{val_loss:.4f}-{val_d_DICE:.4f}-{epoch:03d}.h5' % (net, size,name)
+
+    return keras.callbacks.ModelCheckpoint(filepath, save_weights_only=True, save_best_only=True)
+
 
 def get_args():
     parser = argparse.ArgumentParser(description="net .", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument("--net", "-net",type=str,  help=" net type",default='U2netS')
     parser.add_argument("--batch_size","-batch_size", type=int, default=2, help="batch size")
-    parser.add_argument("--epochs","-epochs", type=int, default=1000, help="number of epochs")
-    parser.add_argument("--size", "-size", type=int, default=288, help=" image size")
+    parser.add_argument("--epochs","-epochs", type=int, default=150, help="number of epochs")
+    parser.add_argument("--size", "-size", type=int, default=256, help=" image size")
     parser.add_argument('--pretrained',"-p",type=str,default=None,help=' pretrained model weights  h5 file')
     parser.add_argument("--learning_rate",'-lr', type=float,default=0.001, help="learning_rate")
     parser.add_argument("--input", '-input', type=str, default='data/gastric.csv', help="train data csv")
-    #parser.add_argument("--warmup", '-wp', type=int, default=1, help="warmup ")
-    #parser.add_argument("--lr_scheduler",'-lrs', type=int,default=2,help="1-stage decay,2-CosineAnnealingScheduler")
+    parser.add_argument("--warmup", '-wp', type=int, default=1, help="warmup ")
+    parser.add_argument("--loss",'-loss', type=str,default='bce_dice_focal',help="loss")
     parser.add_argument("--optimizers", '-opt', type=str, default='SGD', help="optimizers")
+    parser.add_argument("--lrs", '-lrs', type=int, default=0, help="lr decay,1-cos ")
     args = parser.parse_args()
     return args
 if __name__ == '__main__':
@@ -50,42 +51,56 @@ if __name__ == '__main__':
     lr=parsers.learning_rate
     d=pd.read_csv(parsers.input)
     d1=d[d.label==1]
-    d0 = d[d.label == 0].sample(frac=0.35)
+    d0 = d[d.label == 0].sample(frac=0.2)
     d0.type='train'
     d=pd.concat([d0,d1]).sample(frac=1)
     nn=d[d.type=='train'].shape[0]
+    test=d[d.type=='test']
+    test=test[test.label==1]
     net =parsers.net
     opt=parsers.optimizers
-    if net in ['U2netS','U2netM','U2net','Unet3','UEfficientNetB4']:
-        mask_num=6
-    else:
-        mask_num=1
+    wp=parsers.warmup
+    lrs=parsers.lrs
+    model = build_model(size=size, net=net)
+
+    mask_num=len(model.outputs)
+
+    #if net in ['Unet','Deeplabv3']:
+    #    mask_num=1
+    #else:
+    #    mask_num=6
     train_gen=DataGeneratorM(data=d[d.type=='train'],batch_size=batch_size,size=size,au=True,mask_num=mask_num)
-    val_gen=DataGeneratorM(data=d[d.type=='test'],batch_size=batch_size,size=size,shuffle=False,mask_num=mask_num)
+    val_gen=DataGeneratorM(data=test,batch_size=batch_size,size=size,shuffle=False,mask_num=mask_num)
 
     model=build_model(size=size, net=net)
     if parsers.pretrained is not None:
         model.load_weights(parsers.pretrained)
-    model.summary()
+    #model.summary()
 
     if opt=='SGD':
         opt=keras.optimizers.SGD(learning_rate=lr,momentum=0.95,nesterov=True)
     elif opt=='Adam':
-        opt=keras.optimizers.Adam(learning_rate=lr)
+        opt=keras.optimizers.Adam(learning_rate=lr,amsgrad=True)
     elif opt=='SGDW':
-        opt =tfa.optimizers.SGDW(learning_rate=lr,weight_decay=0.00005)
+        opt =tfa.optimizers.SGDW(learning_rate=lr,weight_decay=0.00005,momentum=0.95,nesterov=True)
     elif opt=='AdamW':
-        opt=tfa.optimizers.AdamW(learning_rate=lr,weight_decay=0.00005)
+        opt=tfa.optimizers.AdamW(learning_rate=lr,weight_decay=0.00005,amsgrad=True)
+    #elif opt=='AdamWG':
+    #    AdamW = extend_with_weight_decay(Adam, 'AdamW')
+    #    AdamWG = extend_with_gradient_accumulation(AdamW, 'AdamWG')
+    #    opt = AdamWG(learning_rate=lr,weight_decay_rate=0.0,exclude_from_weight_decay=['Norm', 'bias'],grad_accum_steps=4 )
     else:
         opt=keras.optimizers.SGD(learning_rate=lr)
 
     print(parsers)
+    name=parsers.loss
+    model.compile(optimizer=opt,loss=getLoss(name=name),metrics=[DICE] )
+    #model.compile(optimizer=opt, loss=keras.losses.binary_crossentropy,metrics=[DICE])
+    callbacks_list = [modelSave(net, size=size, name=name,num_mask=mask_num),keras.callbacks.EarlyStopping(patience=50)]
+    if wp>0:
+        callbacks_list=callbacks_list+[WarmUpLearningRateScheduler(warmup_batches=wp*nn//batch_size,init_lr=0.00001)]
+    if lrs>0:
+        callbacks_list=callbacks_list+[CosineAnnealingScheduler(T_max=epochs, eta_max=lr, eta_min=0.000001, verbose=1)]
 
-    filepath='saved/%s_%s_{val_loss:.4f}-{val_d_DICE:.4f}-{epoch:03d}.h5' % (net,size)
-
-    model.compile(optimizer=opt,loss=bce_dice_loss,metrics=[DICE] )
-
-    callbacks_list=[keras.callbacks.ModelCheckpoint(filepath,save_weights_only=True,save_best_only=False)]
-                    #keras.callbacks.TensorBoard(log_dir='./logs',histogram_freq=10)]
-    model.fit(train_gen,epochs=epochs,steps_per_epoch=nn//batch_size,validation_data=val_gen,callbacks=callbacks_list,workers=2 )
+    model.fit(train_gen,epochs=epochs,steps_per_epoch=nn//batch_size,validation_data=val_gen,callbacks=callbacks_list,workers=3 )
 
